325cb6e7126ce71ea3263f33d6f16e0cd4d6033d
chrispy124 <christopher.patel@live.co.uk>
2016-12-02 23:31:31 +0000

    Update Q13 Graph FINISHED.py

--- Q13 Graph FINISHED.py
+++ Q13 Graph FINISHED.py
@@ -4,20 +4,20 @@ class node(object):
         self.node = node
         self.a ={}
 
-    def addOpposite(self, OppositeN):
+    def addOpposite(self, OppositeN): #Adds The Opposite Node
         self.a[OppositeN] = OppositeN
 
-class unweightedGraph:
+class unweightedGraph: #This creates the Graph to use
     def __init__(self):
         self.v = {}
 
-    def addVertex(self,value):
+    def addVertex(self,value): #Adds the vertex
         vertex = node(value)
         self.v[value] = vertex
-        print ("Vertex Created " + str(value))
+        print ("Vertex Created " + str(value)) #This prints the vertex and name
 
     def addEdge(self, value, value2):
-        self.v[value].addOpposite(self.v[value2])
+        self.v[value].addOpposite(self.v[value2]) #This adds the opposite vertex and connects it to the already connected vertex
         self.v[value2].addOpposite(self.v[value])
 
     def printGraph(self):
@@ -31,7 +31,7 @@ class unweightedGraph:
 
 graph = unweightedGraph()
 
-graph.addVertex(1)
+graph.addVertex(1) 
 graph.addVertex(2)
 graph.addVertex(3)
 graph.addVertex(4)
