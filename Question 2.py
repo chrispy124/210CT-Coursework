9333d30c48fcbfdb1fd7bdff5a384aefed4e35e2
chrispy124 <christopher.patel@live.co.uk>
2016-12-02 23:19:39 +0000

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -2,10 +2,10 @@ def ZeroTrail(n): #declares the function
     fives = 0 
 
     for num in range ( 2, n+1):
-        while ((num > 0) == (True)): #while 
-            if num %5 == 0:
+        while ((num > 0) == (True)): #while num being bigger than 0 is true
+            if num %5 == 0: #if num divided by 5 is equal to 0
                 fives +=1
-                num = num/5
+                num = num/5 
             else:
-                break
+                break #breaks the while loop
     return fives
