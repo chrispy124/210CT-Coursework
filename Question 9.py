000442b6bb43fcb7c4ab34064951685a36795935
chrispy124 <christopher.patel@live.co.uk>
2016-12-02 23:34:26 +0000

    Update Question 9.py

--- Question 9.py
+++ Question 9.py
@@ -1,22 +1,19 @@
-def BSearch(List, Number1, Number2):
+def BSearch(List, Number1, Number2): #3 Arguments for function 
     num1 = 0
-    num2 = len(List)-1
+    num2 = len(List)-1 
     FoundNum = False
 
     while num1 <= num2 and not FoundNum:
         middle = (num1 + num2) //2
         if List[middle] > Number1 and List [middle] < Number2:
-            FoundNum = True
+            FoundNum = True #Returns if number is found
 
-        else:
+        else: #IF number is not found then use this code
             if Number1< List[middle]:
                 num2 = middle - 1
             else:
-                num1 = middle + 1
+                num1 = middle + 1 
     return FoundNum
-
-
-
 
 
 print(BSearch([2,3,5,7,9,13],10,14))
